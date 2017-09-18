87c4ce2ceddf1e4c4656469b3fb86714abc24797
NiteshKumar219 <nisris219@gmail.com>
2017-09-18 18:51:50 +0530

    uplode

---
+++ DreamLaptop/DreamLaptopBackEnd/src/test/java/eCae/BackEnd/CategoryTest.java
@@ -0,0 +1,35 @@
+/*package eCae.NisrisBackEnd;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import eCae.NisrisBackEnd.dao.CategoryDao;
+import eCae.NisrisBackEnd.daoImpl.CategoryDaoImpl;
+import eCae.NisrisBackEnd.model.Category;
+
+public class CategoryTest {
+
+	@Autowired
+	CategoryDaoImpl cd;
+	
+	@Test
+	public void test() {
+		
+		/*AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+		ctx.scan("eCae.NisrisBackEnd");
+       ctx.refresh();
+       CategoryDao cd = (CategoryDao)ctx.getBean(CategoryDao.class);
+       
+		
+       Category c = new Category();
+       c.setCid(12455);
+       c.setName("Dell");
+	assertTrue(cd.addCategory(c));
+	}
+	
+
+}
+*/
